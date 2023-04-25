bc1902232b606734a40e34af1fab98e69bf92d53
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-26 01:12:54 +0330

    inital commit

---
+++ RentalProject/src/Movie.java
@@ -0,0 +1,10 @@
+import java.sql.Date;
+
+public class Movie {
+    private static int ID;
+    public String title;
+    public String genre;
+    public String director;
+    public Date releaseDate;
+    public Boolean isAvailable;
+}
---
+++ RentalProject/src/Rental.java
@@ -0,0 +1,3 @@
+public class Rental {
+    private static int ID;
+}
