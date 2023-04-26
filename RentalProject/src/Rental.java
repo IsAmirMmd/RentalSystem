5ad3f9e1e3e21c3651a1926cd8187e1eb56b6fdb
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-26 22:50:50 +0330

    Rental finished

--- RentalProject/src/Rental.java
+++ RentalProject/src/Rental.java
@@ -8,4 +8,42 @@ public class Rental {
     public Date rentalDate;
     public Date returnDate;
 
+    public Rental(Movie movie, Customer customer, int ID) {
+        this.ID = ID;
+        this.movie = movie;
+        this.customer = customer;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String calculateFee() {
+        long timeDistance = returnDate.getTime() - rentalDate.getTime();
+        return ("Your fee is " + timeDistance);
+    }
 }
