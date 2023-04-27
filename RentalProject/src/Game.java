365a88bc51e344ee81cb23e540820f3a0f7bef9b
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-27 17:44:20 +0330

    Game class finished

---
+++ RentalProject/src/Game.java
@@ -0,0 +1,51 @@
+import java.util.Date;
+
+public class Game extends Item {
+    public String title;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public Date releaseDate;
+    public Boolean isAvailable;
+    public long ID;
+
+    public Game(String title, String genre, String platform, String publisher, Date releaseDate, long ID) {
+        super(title, genre, releaseDate, ID);
+        this.platform = platform;
+        this.publisher = publisher;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getplatform() {
+        return platform;
+    }
+
+    public String getpublisher() {
+        return publisher;
+    }
+
+    @Override
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    @Override
+    public long getID() {
+        return ID;
+    }
+
+    @Override
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+}
