250418dee054c645401653c3b6aa53736f6743d2
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-27 17:45:28 +0330

    Book class finished

---
+++ RentalProject/src/Book.java
@@ -0,0 +1,52 @@
+import java.util.Date;
+
+class Book extends Item {
+
+    public String title;
+    public String genre;
+    public String author;
+    public String publisher;
+    public Date releaseDate;
+    public Boolean isAvailable;
+    public long ID;
+
+    public Book(String title, String genre, String author, String publisher, Date releaseDate, long ID) {
+        super(title, genre, releaseDate, ID);
+        this.author = author;
+        this.publisher = publisher;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getauthor() {
+        return author;
+    }
+
+    public String getpublisher() {
+        return publisher;
+    }
+
+    @Override
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    @Override
+    public long getID() {
+        return ID;
+    }
+
+    @Override
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+}
