f5bb61c6aacc15473086b159f08eff0529d874f8
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-27 17:36:06 +0330

    completing Item class

--- RentalProject/src/Item.java
+++ RentalProject/src/Item.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 
 public class Item {
+    public Boolean isAvailable = false;
     public String title;
     public String genre;
     public Date releaseDate;
@@ -11,5 +12,30 @@ public class Item {
         this.genre = genre;
         this.releaseDate = releaseDate;
         this.ID = ID;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
     }
 }
