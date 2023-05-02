899e09452a9038e62b861402db3580e9b632bfb2
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-05-02 18:37:23 +0330

    rental list updated for customers

--- RentalProject/src/Customer.java
+++ RentalProject/src/Customer.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
 
 public class Customer {
-    public int ID;
+    public long ID;
     public String name;
     public String email;
     public String phone;
     public String address;
     public ArrayList<Rental> rentals;
 
-    public Customer(String name, String email, String phone, String address, int ID) {
+    public Customer(String name, String email, String phone, String address, long ID) {
         this.ID = ID;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.address = address;
-        for (Rental temp : RentalStore.rentalList) {
-            if (temp.customer.ID == this.ID) {
-                this.rentals.add(temp);
-            }
-        }
+        this.rentals = new ArrayList<>();
     }
 
-    public int getID() {
+    public long getID() {
         return ID;
     }
 
@@ -42,6 +38,16 @@ public class Customer {
     }
 
     public ArrayList<Rental> getRentals() {
+        System.out.println(this.name + " rent this book :");
+        rentals = new ArrayList<>();
+        int i = 1;
+        for (Rental temp : RentalStore.rentalList) {
+            if (temp.customer.ID == this.ID) {
+                rentals.add(temp);
+                System.out.println(i + ". " + temp.movie.title);
+                i++;
+            }
+        }
         return rentals;
     }
 }
